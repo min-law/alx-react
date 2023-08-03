d9f943fc3de8c8a77953ca5e92b06e80c0464f33
lawrence Akinboye <51114125+min-law@users.noreply.github.com>
2023-08-03 15:48:52 +0100

    Update App.js

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -1,46 +1,75 @@
-import React, { Component, Fragment } from 'react';
-import Header from '../Header/Header';
+import  React from 'react';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
-import Notifications from '../Notifications/Notifications';
-import CourseList from '../CourseList/CourseList';
+import Header from '../Header/Header';
 import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
 const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
-];
-const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+  {id: 1, name: 'ES6', credit: 60},
+  {id: 2, name: 'Webpack', credit: 20},
+  {id: 3, name: 'React', credit: 40}
 ];
 
-class App extends Component {
+const listNotifications = [
+  {id: 1, type: 'default', value: 'New course available'},
+  {id: 2, type: 'urgent', value: 'New resume available'},
+  {id: 3, type: 'urgent', html: { __html: getLatestNotification() }}
+];
+
+class App extends React.Component {
   constructor(props) {
     super(props);
+    this.keyboardKeys = this.keyboardKeys.bind(this);
   }
+
+  keyboardKeys(x) {
+    if (x.key === 'h' && x.ctrlKey) {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.keyboardKeys);
+  }
+
+  componentWillUnmount() {
+    document.addEventListener('keydown', this.keyboardKeys);
+  }
+
   render() {
-    const { isLoggedIn } = this.props;
+    const { isLoggedIn, logOut } = this.props;
     return (
-      <Fragment>
-        <Notifications listNotifications={listNotifications} />
-        <Header />
-        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-        <Footer />
-      </Fragment>
+      <React.Fragment>
+        <Notifications listNotifications={listNotifications}/>
+        <div className="App">
+          <Header />
+        </div>
+        <div className="App-body">
+          {
+            isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login />
+          }
+        </div>
+        <div className="App-footer">
+          <Footer />
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-App.defaultProps = {
-  isLoggedIn: false,
-};
-
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {}
 };
 
 export default App;
